bbf3366272f23280f26034df0fe7864b460d4848
Tejas Chapke <162096588+tejaschapke18@users.noreply.github.com>
2024-10-25 20:30:49 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,22 +5,17 @@ import firebase from "firebase/compat/app";
 import { AuthProvider } from "./components/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC7DLM6rspr6JgoooPkTE1Zb2QZXdcjxQo",
-  authDomain: "blogs-93765.firebaseapp.com",
-  projectId: "blogs-93765",
-  storageBucket: "blogs-93765.appspot.com",
-  messagingSenderId: "911912315585",
-  appId: "1:911912315585:web:9f2c8a7e77a1a8610c4240",
-};
+//firebase config
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
+const clientId = process.env.GOOGLE_OAUTH_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <AuthProvider>
-      <GoogleOAuthProvider clientId="287908866352-0pjr401q6fg1d0ko47429g371j7mkm5f.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId=clientId>
         <App />
       </GoogleOAuthProvider>
     </AuthProvider>
