86cbab3ef6e7805375b9e2a20cb4f95ac1aa032a
Tejas Chapke <162096588+tejaschapke18@users.noreply.github.com>
2024-10-25 20:27:32 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,14 +34,8 @@ app.use(bodyParser.json());
 app.use(limiter);
 app.use(logRequest);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC7DLM6rspr6JgoooPkTE1Zb2QZXdcjxQo",
-  authDomain: "blogs-93765.firebaseapp.com",
-  projectId: "blogs-93765",
-  storageBucket: "blogs-93765.appspot.com",
-  messagingSenderId: "911912315585",
-  appId: "1:911912315585:web:9f2c8a7e77a1a8610c4240",
-};
+//firebase config
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
